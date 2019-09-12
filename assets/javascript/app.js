4c83afbbfd51423a648cff4fe210cf282b3f964c
Sean Richter <sean.richter04@outlook.com>
2019-09-11 19:53:58 -0500

    firebase configuration added to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyDo0XfpVHctEv9A38T5OGEIK-aFPGHPT68",
+        authDomain: "train-scheduler-ba6c4.firebaseapp.com",
+        databaseURL: "https://train-scheduler-ba6c4.firebaseio.com",
+        projectId: "train-scheduler-ba6c4",
+        storageBucket: "",
+        messagingSenderId: "881794318336",
+        appId: "1:881794318336:web:631d8ded14e539d523d8e5"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    
