7d186480a283853c0100c70d09b1569f6c8ddd17
Sean Richter <sean.richter04@outlook.com>
2019-09-13 10:06:56 -0500

    trying to figure out why database is not recognized

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,17 @@
 console.log("hello world");
 // Your web app's Firebase configuration
-var config = {
-apiKey: "AIzaSyDo0XfpVHctEv9A38T5OGEIK-aFPGHPT68",
-authDomain: "train-scheduler-ba6c4.firebaseapp.com",
-databaseURL: "https://train-scheduler-ba6c4.firebaseio.com",
-projectId: "train-scheduler-ba6c4",
-storageBucket: "gs://train-scheduler-ba6c4.appspot.com/",
-messagingSenderId: "881794318336",
-appId: "1:881794318336:web:631d8ded14e539d523d8e5"
+const firebaseConfig = {
+  apiKey: "AIzaSyDo0XfpVHctEv9A38T5OGEIK-aFPGHPT68",
+  authDomain: "train-scheduler-ba6c4.firebaseapp.com",
+  databaseURL: "https://train-scheduler-ba6c4.firebaseio.com",
+  projectId: "train-scheduler-ba6c4",
+  storageBucket: "train-scheduler-ba6c4.appspot.com",
+  messagingSenderId: "881794318336",
+  appId: "1:881794318336:web:631d8ded14e539d523d8e5"
 };
-
 // Initialize Firebase
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
-var firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/database');
 // Database
 var database = firebase.database();
 
