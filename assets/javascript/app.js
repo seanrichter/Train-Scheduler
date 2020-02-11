182be857e9fbc815c5ef13c242e95ed455420dbd
Sean Richter <sean.richter04@outlook.com>
2020-02-10 21:08:34 -0600

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,18 @@
 console.log("hello world");
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDo0XfpVHctEv9A38T5OGEIK-aFPGHPT68",
-  authDomain: "train-scheduler-ba6c4.firebaseapp.com",
-  databaseURL: "https://train-scheduler-ba6c4.firebaseio.com",
-  projectId: "train-scheduler-ba6c4",
-  storageBucket: "train-scheduler-ba6c4.appspot.com",
-  messagingSenderId: "881794318336",
-  appId: "1:881794318336:web:631d8ded14e539d523d8e5"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const admin = require("firebase-admin");
-admin.initializeApp();
+var firebaseConfig = {
+    apiKey: "AIzaSyDo0XfpVHctEv9A38T5OGEIK-aFPGHPT68",
+    authDomain: "train-scheduler-ba6c4.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ba6c4.firebaseio.com",
+    projectId: "train-scheduler-ba6c4",
+    storageBucket: "train-scheduler-ba6c4.appspot.com",
+    messagingSenderId: "881794318336",
+    appId: "1:881794318336:web:cafdb3905ebb7e3523d8e5",
+    measurementId: "G-ZSSHR8SRF1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 // Database
 var database = firebase.database();
