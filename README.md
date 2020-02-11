4d6f81cb684995b3fa69727c5fe8d0927420772f
Sean Richter <sean.richter04@outlook.com>
2020-02-10 20:24:03 -0600

    updated indenting and readme

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Train-Scheduler
-Create a train schedule app that incorporates firebase to host arrival and departure data.  The app will retrieve and manipulate this information with Moment.js.  This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until the arrive at their station.
+# Firebase-Train-Scheduler
+[Firebase database train scheduler](https://seanrichter.github.io/Train-Scheduler/)
+
+**Object**
+
+The goal of this assignment is to:
+1. Set up a Firebase key to store data
+1. Create a front end in which user can enter data, which in turn populates train schedule
+1. Use Moment.js to create formulas for minutes til arrival, etc.
+
+Because this is backend, rather than client-side storage, the data persists regardless of browser, platform, etc.
+
+***Technology used***
+
+This pages uticlizes HTML, CSS, JavaScript, jQuery, Bootstrap, Firebase and the Moment.js library.
+
+I'll be honest. This one threw me for a loop. In class we'd been calling APIs with AJAX and extracting from the JSON-formatted data what we wanted on the screen, e.g. GIPHYs, movie summaries, etc. Like so:
+
+    $.ajax({
+    url: queryURI,
+    method: "GET"
+    }).then(function (response) {
+    for (var i = 0; i < response.data.length; i++) {
+
+For this assignment, rather than calling an existing database, we created our own that user input is stored in:
+
+    database.ref().push(newTrain);
+
+
+    database.ref().on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val());
+
+Children ... snapshots ... childSnapshots ... the syntax sounds so lighthearted. But I've learned through this assignment that Firebase is a powerful tool and -- once you can think of it as a big 'ol filing cabinet with lots of subfolders -- it's not so intimidating after all.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,82 +1,119 @@
 console.log("hello world");
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDo0XfpVHctEv9A38T5OGEIK-aFPGHPT68",
-    authDomain: "train-scheduler-ba6c4.firebaseapp.com",
-    databaseURL: "https://train-scheduler-ba6c4.firebaseio.com",
-    projectId: "train-scheduler-ba6c4",
-    storageBucket: "train-scheduler-ba6c4.appspot.com",
-    messagingSenderId: "881794318336",
-    appId: "1:881794318336:web:631d8ded14e539d523d8e5"
+  apiKey: "AIzaSyDo0XfpVHctEv9A38T5OGEIK-aFPGHPT68",
+  authDomain: "train-scheduler-ba6c4.firebaseapp.com",
+  databaseURL: "https://train-scheduler-ba6c4.firebaseio.com",
+  projectId: "train-scheduler-ba6c4",
+  storageBucket: "train-scheduler-ba6c4.appspot.com",
+  messagingSenderId: "881794318336",
+  appId: "1:881794318336:web:631d8ded14e539d523d8e5"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 // Database
 var database = firebase.database();
 
 // FIREBASE WATCHER + INITIAL LOADER - updates or snapshot everytime a child is added to database
-database.ref().on("child_added", function (childSnapshot) { // creates the row in the table with the values from the firebase database
-    var row = $("<tr>");
-    row.html("<td>" + childSnapshot.val().name + "</td>" + "<td>" + childSnapshot.val().city + "</td><td>" + childSnapshot.val().rate + "</td><td>" + childSnapshot.val().arrival + "</td><td>" + childSnapshot.val().minutes + "</td>" + "<td>" + "<button class='remove btn btn-default'>Remove</button>" + "</td>");
-    $("#table").append(row);
+database.ref().on("child_added", function(childSnapshot) {
+  // creates the row in the table with the values from the firebase database
+  var row = $("<tr>");
+  row.html(
+    "<td>" +
+      childSnapshot.val().name +
+      "</td>" +
+      "<td>" +
+      childSnapshot.val().city +
+      "</td><td>" +
+      childSnapshot.val().rate +
+      "</td><td>" +
+      childSnapshot.val().arrival +
+      "</td><td>" +
+      childSnapshot.val().minutes +
+      "</td>" +
+      "<td>" +
+      "<button class='remove btn btn-default'>Remove</button>" +
+      "</td>"
+  );
+  $("#table").append(row);
 });
 
+$("#submit-train").on("click", function() {
+  event.preventDefault();
 
-$("#submit-train").on("click", function () {
+  var trainName = $("#name")
+    .val()
+    .trim();
+  var destination = $("#destination")
+    .val()
+    .trim();
+  var firstTrain = $("#first-train")
+    .val()
+    .trim();
+  var tFrequency = $("#frequency")
+    .val()
+    .trim();
 
-    event.preventDefault();
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTrain, "hh:mm")
+    .subtract(1, "years")
+    .toString();
+  console.log(firstTimeConverted);
 
-    var trainName = $("#name").val().trim();
-    var destination = $("#destination").val().trim();
-    var firstTrain = $("#first-train").val().trim();
-    var tFrequency = $("#frequency").val().trim();
+  // // current time
+  var currentTime = moment().format();
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years").toString();
-    console.log(firstTimeConverted);
+  // Difference between the times
+  var diffTime = moment().diff(firstTimeConverted, "minutes");
 
-    // // current time
-    var currentTime = moment().format();
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
 
-    // Difference between the times
-    var diffTime = moment().diff(firstTimeConverted, "minutes");
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
 
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
+  // Next Train
+  var nextTrain = moment()
+    .add(tMinutesTillTrain, "minutes")
+    .format("hh:mm");
 
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
+  // Pushing values to the firebase database
+  database
+    .ref()
+    .set({
+      name: trainName,
+      city: destination,
+      rate: tFrequency,
+      arrival: nextTrain,
+      minutes: tMinutesTillTrain
+    });
 
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+  // after submit set the value of each input field to an empty string
 
-    // Pushing values to the firebase database
-    database.ref().set({name: trainName, city: destination, rate: tFrequency, arrival: nextTrain, minutes: tMinutesTillTrain});
-
-
-    // after submit set the value of each input field to an empty string
-
-    $("#name").val(" ");
-    $("#destination").val(" ");
-    $("#first-train").val(" ");
-    $("#frequency").val(" ");
-
+  $("#name").val(" ");
+  $("#destination").val(" ");
+  $("#first-train").val(" ");
+  $("#frequency").val(" ");
 });
 
+$(document.body).on("click", ".remove", function() {
+  // get constant trId / database ID
+  var buttonGradparentId = $(this)
+    .parent()
+    .parent()
+    .attr("id");
 
-$(document.body).on("click", ".remove", function () { // get constant trId / database ID
-    var buttonGradparentId = $(this).parent().parent().attr("id");
+  // remove from database
+  var removeRef = database.ref(buttonGradparentId);
+  removeRef.remove();
 
-    // remove from database
-    var removeRef = database.ref(buttonGradparentId);
-    removeRef.remove();
-
-
-    // remove from HTML
-    $(this).parent().parent().remove();
-
+  // remove from HTML
+  $(this)
+    .parent()
+    .parent()
+    .remove();
 });
